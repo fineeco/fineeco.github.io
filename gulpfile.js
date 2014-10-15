0afb9c5429f29f6af730b6555ee1ac7b767555ab
dididi <dfdgsdfg@gmail.com>
2014-10-16 00:16:49 +0900

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -41,12 +41,12 @@ gulp.task('html', ['styles', 'scripts'], function () {
 
 gulp.task('images', function () {
     return gulp.src('app/images/**/*')
-        //.pipe($.cache($.imagemin({
-        .pipe($.imagemin({
+        pipe($.cache($.imagemin({
+        // .pipe($.imagemin({
             optimizationLevel: 3,
             progressive: true,
             interlaced: true
-        }))
+        })))
         .pipe(gulp.dest('dist/images'))
         .pipe($.size());
 });
