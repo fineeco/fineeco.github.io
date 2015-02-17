458d2439c0d7afc12f7b0eb726ab55a46903a2b1
dididi <dfdgsdfg@gmail.com>
2015-02-18 00:10:21 +0900

    beautify

--- app/scripts/nibiru-skip-link-focus-fix.js
+++ app/scripts/nibiru-skip-link-focus-fix.js
@@ -1,20 +1,20 @@
 (function() {
-	'use strict';
-	var isWebkit = navigator.userAgent.toLowerCase().indexOf('webkit') > -1,
-		isOpera = navigator.userAgent.toLowerCase().indexOf('opera') > -1,
-		isIE = navigator.userAgent.toLowerCase().indexOf('msie') > -1;
+  'use strict';
+  var isWebkit = navigator.userAgent.toLowerCase().indexOf('webkit') > -1,
+    isOpera = navigator.userAgent.toLowerCase().indexOf('opera') > -1,
+    isIE = navigator.userAgent.toLowerCase().indexOf('msie') > -1;
 
-	if ((isWebkit || isOpera || isIE) && 'undefined' !== typeof(document.getElementById)) {
-		var eventMethod = (window.addEventListener) ? 'addEventListener' : 'attachEvent';
-		window[eventMethod]('hashchange', function() {
-			var element = document.getElementById(location.hash.substring(1));
+  if ((isWebkit || isOpera || isIE) && 'undefined' !== typeof(document.getElementById)) {
+    var eventMethod = (window.addEventListener) ? 'addEventListener' : 'attachEvent';
+    window[eventMethod]('hashchange', function() {
+      var element = document.getElementById(location.hash.substring(1));
 
-			if (element) {
-				if (!/^(?:a|select|input|button|textarea)$/i.test(element.tagName)) {
-					element.tabIndex = -1;
-				}
-				element.focus();
-			}
-		}, false);
-	}
+      if (element) {
+        if (!/^(?:a|select|input|button|textarea)$/i.test(element.tagName)) {
+          element.tabIndex = -1;
+        }
+        element.focus();
+      }
+    }, false);
+  }
 })();
