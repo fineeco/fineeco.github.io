506d8f4b0b04b1ec8d715cd80c095cb0c5f89c56
dididi <dfdgsdfg@gmail.com>
2015-02-25 12:27:54 +0900

    write product page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,4 +7,18 @@ $(document).ready(function() {
 	      }
 	});
 
+	(function() {
+		var mySlider = new Slider('#ex13', {
+			ticks: [0, 100, 200, 300, 400, 500],
+			ticks_labels: ['2mm', '5mm', '10mm', '20mm', '30mm', '50mm'],
+			ticks_snap_bounds: 30
+		});
+	}());
+
+	// $('#ex13').bootstrapSlider({
+	//     ticks: [0, 100, 200, 300, 400],
+	//     ticks_labels: ['$0', '$100', '$200', '$300', '$400'],
+	//     ticks_snap_bounds: 30
+	// });
+
 });
