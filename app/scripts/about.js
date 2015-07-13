14a5b2b7ece39bd8cd69ee9662fc3a0844475702
dididi <dfdgsdfg@gmail.com>
2015-07-14 02:05:09 +0900

    update about iso images

--- app/scripts/about.js
+++ app/scripts/about.js
@@ -12,8 +12,16 @@ var changeStyleByLng = function() {
   var currentLng = i18n.lng();
   if (currentLng === 'en-US') {
     $('.name').attr('class', 'name lng-en');
+    $('.iso9001').attr('src', 'images/iso9001-en.jpg');
+    $('.iso9001-wrapper').attr('href', 'images/iso9001-en.jpg');
+    $('.iso14001').attr('src', 'images/iso14001-en.jpg');
+    $('.iso14001-wrapper').attr('href', 'images/iso14001-en.jpg');
   } else if (currentLng === 'ko-KR') {
     $('.name').attr('class', 'name lng-ko');
+    $('.iso9001').attr('src', 'images/iso9001-ko.jpg');
+    $('.iso9001-wrapper').attr('href', 'images/iso9001-ko.jpg');
+    $('.iso14001').attr('src', 'images/iso14001-ko.jpg');
+    $('.iso14001-wrapper').attr('href', 'images/iso14001-ko.jpg');
   } else {
     return false;
   }
